e4dcbe00d932f39bcdf52ab3162d80e0617ed0ec
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-06 10:48:49 +0100

    upload

--- src/store/index.js
+++ src/store/index.js
@@ -3,19 +3,37 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     todos: [
-      { id: 1, text: 'Realizar el curso de Nest', completed: true},
-      { id: 2, text: 'Realizar el curso de Angular Avanzado', completed: false},
-      { id: 3, text: 'Realizar el curso de React', completed: false},
-      { id: 4, text: 'Realizar el curso de React Avanzado', completed: false},
-      { id: 5, text: 'Realizar el curso de React Native', completed: false},
+      { id: 1, text: 'Realizar el curso de Nest', completed: true },
+      { id: 2, text: 'Realizar el curso de Angular Avanzado', completed: false },
+      { id: 3, text: 'Realizar el curso de React', completed: false },
+      { id: 4, text: 'Realizar el curso de React Avanzado', completed: false },
+      { id: 5, text: 'Realizar el curso de React Native', completed: false },
     ]
   },
   getters: {
     pendingTodos(state) {
-      return state.todos.filter( todo => !todo.completed )
+      return state.todos.filter(todo => !todo.completed)
+    },
+    completedTodos(state) {
+      return state.todos.filter(todo => todo.completed)
+    },
+    allTodos(state) {
+      return state.todos
+    },
+    getTodosByTab: (_, getters) => (tab) => {
+      switch (tab) {
+        case 'all': return getters.allTodos
+        case 'pending': return getters.pendingTodos
+        case 'completed': return getters.completedTodos
+      }
     }
   },
   mutations: {
+    toggleTodo(state, id) {
+      console.log('hola');
+      const todoIdx = state.todos.findIndex(todo => todo.id === id)
+      state.todos[todoIdx].completed = !state.todos[todoIdx].completed
+    }
   },
   actions: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,20 +4,51 @@
     <hr>
     <h4>Pendientes: {{ pendingTodos.length }}</h4>
     <hr>
+    <button type="button" class="btn btn-warning m-2" :class="{ 'active': currenTab === 'all' }"
+      @click="currenTab = 'all'">Todos</button>
+    <button type="button" class="btn btn-warning m-2" :class="{ 'active': currenTab === 'pending' }"
+      @click="currenTab = 'pending'">Pendientes</button>
+    <button type="button" class="btn btn-warning m-2" :class="{ 'active': currenTab === 'completed' }"
+      @click="currenTab = 'completed'">Completados</button>
+    <hr>
+    <ul>
+      <li v-for="todo in getTodosByTab" :class="{ 'completed': todo.completed }" :key="todo.id"
+        @dblclick="toggleTodo(todo.id)">{{ todo.text }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
 export default {
   name: 'HomeView',
-  setup () {
+  setup() {
     const store = useStore()
+    const currenTab = ref('all')
 
     return {
+      currenTab,
       pendingTodos: computed(() => store.getters['pendingTodos']),
+      getTodosByTab: computed(() => store.getters['getTodosByTab'](currenTab.value)),
+      toggleTodo: (id) => { store.commit('toggleTodo', id) }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+li {
+  cursor: pointer;
+  font-size: 1.2em;
+}
+
+.active {
+  color: white;
+  background-color: #750e26;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+</style>
